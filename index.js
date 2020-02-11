6d5fe044de5471ea0e6377b91aad034cc0f572e7
Andreas Due Pihl <6140251@RTS-365.dk>
2020-02-11 14:05:49 +0100

    fetch

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 // foo()
 
-const keepo = document.querySelector("kappa")
 
+// function keepo() { document.querySelector("body").innerHTML = hrj }
+// keepo()
+
+fetch("https://acc", {
+    method: "POST",
+    withCredentials: true,
+    headers: {
+
+        "Content-Type": "application/x-www-form-urlencoded"
+
+    }
+})
